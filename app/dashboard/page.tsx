6a2ac72eada4be165e1f470aace123913b7463da
Apoorv <apoorvarnav1@gmail.com>
2026-09-07 13:44:02 +0530

    chore: fix-redirects

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation"
 
 export default function Page() {
-  redirect("/agents")
+  redirect("/dashboard/agents")
   // return (
   //   <div className="flex flex-1 flex-col">
   //     <div className="@container/main flex flex-1 flex-col gap-2">
